873f59a276a3dae5a277efea3f783e636831570c
Tanishka khamesara <tanishkajain24941@gmail.com>
2024-04-19 22:16:50 +0530

    progress bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [msg, setMsg] = useState("Loading....");
+
+  useEffect(() => {
+    let inter = setInterval(() => {
+      if (number < 100) {
+        setNumber((prev) => prev + 1);
+      } else {
+        clearInterval(inter);
+        setMsg("Completed😎");
+      }
+    }, 300);
+    return ()=> clearInterval(inter);
+  },[number])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Progress Bar</h1>
+      <div className="container">
+        <div className="bar" style={{ width: `${number}%`, backgroundColor: "green" }}></div>
+      </div>
+      <p>{msg}</p>
     </div>
   );
 }
